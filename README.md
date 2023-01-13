f7e721dd1b6d5bcac25f0e48627cfbdc015309f3
web3sdks-template <developtemplate@web3sdks.com>
2023-01-13 11:10:58 +0800

    First commit

---
+++ README.md
@@ -0,0 +1,32 @@
+## Solana frontend started kit
+
+This template has everything you need to build a web3 app on Solana using the web3sdks SDK.
+
+## Getting Started
+
+Create a project using this example:
+
+```bash
+npx web3sdks create --template next-typescript-solana-starter
+```
+
+You can start editing the page by modifying `pages/index.tsx`. The page auto-updates as you edit the file.
+
+On `pages/_app.tsx`, you'll find our `Web3sdksProvider` wrapping your app, this is necessary for our hooks to work.
+
+on `pages/index.tsx`, you'll find an example of a connect wallet button and some hooks.
+
+## Learn More
+
+To learn more about web3sdks and Next.js, take a look at the following resources:
+
+- [web3sdks React Documentation](https://docs.web3sdks.com/react) - learn about our React SDK.
+- [web3sdks TypeScript Documentation](https://docs.web3sdks.com/typescript) - learn about our JavaScript/TypeScript SDK.
+- [web3sdks Portal](https://docs.web3sdks.com) - check our guides and development resources.
+- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
+
+You can check out [the web3sdks GitHub organization](https://github.com/web3sdks) - your feedback and contributions are welcome!
+
+## Join our Discord!
+
+For any questions, suggestions, join our discord at [https://discord.gg/KX2tsh9A](https://discord.gg/KX2tsh9A).
---
+++ pages/index.tsx
@@ -0,0 +1,67 @@
+import type { NextPage } from "next";
+import dynamic from "next/dynamic";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+
+// Default styles that can be overridden by your app
+require("@solana/wallet-adapter-react-ui/styles.css");
+
+const WalletMultiButtonDynamic = dynamic(
+  async () =>
+    (await import("@solana/wallet-adapter-react-ui")).WalletMultiButton,
+  { ssr: false }
+);
+
+const Home: NextPage = () => {
+  // Here's how to get the web3sdks SDK instance
+  // const sdk = useSDK();
+  // Here's how to get a nft collection
+  // const { program } = useProgram(
+  //   your_nft_collection_address,
+  //   "nft-collection"
+  // );
+
+  return (
+    <>
+      <div className={styles.container}>
+        <div className={styles.iconContainer}>
+          <Image
+            src="/web3sdks.svg"
+            height={75}
+            width={115}
+            style={{
+              objectFit: "contain",
+            }}
+            alt="web3sdks"
+          />
+          <Image
+            width={75}
+            height={75}
+            src="/sol.png"
+            className={styles.icon}
+            alt="sol"
+          />
+        </div>
+        <h1 className={styles.h1}>Solana, meet web3sdks 👋</h1>
+        <p className={styles.explain}>
+          Explore what you can do with web3sdks&rsquo;s brand new{" "}
+          <b>
+            <a
+              href="https://docs.web3sdks.com/solana"
+              target="_blank"
+              rel="noopener noreferrer"
+              className={styles.lightPurple}
+            >
+              Solana SDK
+            </a>
+          </b>
+          .
+        </p>
+
+        <WalletMultiButtonDynamic />
+      </div>
+    </>
+  );
+};
+
+export default Home;
